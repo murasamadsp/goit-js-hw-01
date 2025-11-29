44ec2e0beb5e437df56c8185c1b3ecdc8cedc0df
Mykhailo Ostrovskyi <murasama.off@gmail.com>
2025-11-29 23:45:09 +0200

    v2

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ function makeTransaction(quantity, pricePerDroid) {
   let totalPrice = quantity * pricePerDroid;
 
   return (
-    "You ordered " + quantity + " droids worth " + totalPrice + " credits!"
+    'You ordered ' + quantity + ' droids worth ' + totalPrice + ' credits!'
   );
 }
 
